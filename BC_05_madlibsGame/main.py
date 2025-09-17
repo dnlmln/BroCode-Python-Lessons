423e669006d1ba2b6f370d442ef3f0673ba33c92
Daniel Milani <daniel.milani.71@gmail.com>
2025-09-17 13:26:19 -0400

    Lesson 5

---
+++ BC_05_madlibsGame/main.py
@@ -0,0 +1,12 @@
+# Madlibs game
+
+adj1=input("Enter an adjective (description): ")
+noun1=input("Enter a noun (person, place, thing): ")
+adj2=input("Enter a second adjective (description): ")
+verb1=input("Enter a verb (action ending in 'ing'): ")
+adj3=input("Enter a third adjective (description): ")
+
+print(f"Today I went to a {adj1} zoo.")
+print(f"In an exhibit i saw a {noun1}.")
+print(f"The {noun1} was {adj2} and {verb1}")
+print(f"I was {adj3}")
